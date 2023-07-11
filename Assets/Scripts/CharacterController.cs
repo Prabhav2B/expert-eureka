1c84ffef4e74682c93e7a3c3fee9280487e9b757
Prabhav Bhatnagar <prabhav2b@gmail.com>
2023-07-11 22:49:58 +0300

    second jump implementation

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [Header("Jump Implementation")] [SerializeField]
+    private ProjectEnums.JumpType jumpType;
+
+    [Header("Physics Jump Parameter")] 
+    [Range(0, 100)]
+    [SerializeField] private float jumpForce;
+    [Range(0, 100)]
+    [SerializeField] private float gravity = 9.8f;
+    
+    [Header("Quasi Manual Jump Parameter")] 
+    [Range(0, 100)]
+    [SerializeField] private float jumpHeight;
+    [Range(0, 100)]
+    [SerializeField] private float maxGravityAcceleration = 9.8f;
+
+    
     private Rigidbody2D _rb;
     private bool _grounded;
 
@@ -12,6 +30,9 @@ public class PlayerController : MonoBehaviour
     {
         _grounded = true;
         _rb = GetComponent<Rigidbody2D>();
+
+        if(jumpType == ProjectEnums.JumpType.EnginePhysics)
+        Physics2D.gravity = new Vector2(0, -gravity);
     }
 
     // Update is called once per frame
@@ -25,21 +46,37 @@ public class PlayerController : MonoBehaviour
         
         if (!_grounded) return;
         _grounded = false;
-
+        
+        //reset y-velocity for consistency
         _rb.velocity = new Vector2(_rb.velocity.x, 0f);
         
-        var jumpVec = Vector2.up * 10f;
+        var jumpVec = Vector2.up * jumpForce;
         _rb.AddForce(jumpVec, ForceMode2D.Impulse);
     }
 
     void JumpBasic()
     {
+        
+        if (!_grounded) return;
+        _grounded = false;
+
+        //reset y-velocity for consistency
+        _rb.velocity = new Vector2(_rb.velocity.x, 0f);
+                
+        //formula to reach height <jumpHeight>
+        //under gravity <maxGravityAcceleration> 
+        // v0=sqrt(2gY)
+        var jumpVelocity = Mathf.Sqrt(2f * maxGravityAcceleration * jumpHeight);
+        _rb.velocity = new Vector2(_rb.velocity.x, jumpVelocity);
     }
 
     public void PlayerHitGround(int id)
     {
-        
         _grounded = true;
-        JumpPhysics();
+        
+        if(jumpType == ProjectEnums.JumpType.EnginePhysics)
+            JumpPhysics();
+        else
+            JumpBasic();
     }
 }
---
+++ Assets/Scripts/ProjectEnums.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectEnums 
+{
+    public enum JumpType
+    {
+        EnginePhysics,
+        QuasiCustomPhysics
+    }
+}
